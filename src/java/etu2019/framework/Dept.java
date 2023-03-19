94d0687428910b7f5a546fd4f3c7185881fbc016
koloina06 <koloina.mihajatiana@gmail.com>
2023-03-19 09:15:52 +0300

    modif sprint3

---
+++ src/java/etu2019/framework/Dept.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2019.framework;
+
+import etu2019.framework.annotation.App;
+
+/**
+ *
+ * @author koloina
+ */
+public class Dept {
+    
+     @App(url="dept-all")
+    public void findAll(){
+    
+    }
+}
