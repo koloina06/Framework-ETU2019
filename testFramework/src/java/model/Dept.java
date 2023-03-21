f24fa52a7b8a6756336f487e87819539c2cff02e
koloina06 <koloina.mihajatiana@gmail.com>
2023-03-21 09:23:34 +0300

    sprint4

--- testFramework/src/java/model/Dept.java
+++ testFramework/src/java/model/Dept.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package etu2019.framework;
+package model;
 
-import etu2019.framework.annotation.App;
+
 
 /**
  *
