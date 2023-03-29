8a080653988ba19a039f85818f8a330a7c3e54c3
koloina06 <koloina.mihajatiana@gmail.com>
2023-03-29 20:33:42 +0300

    sprint5

---
+++ framework/src/java/etu2019/framework/ModelView.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2019.framework;
+
+/**
+ *
+ * @author koloina
+ */
+public class ModelView {
+    String view;
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+    
+    
+}
--- framework/src/java/etu2019/framework/servlet/FrontServlet.java
+++ framework/src/java/etu2019/framework/servlet/FrontServlet.java
@@ -14,7 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.*;
    import java.util.HashMap;
 import etu2019.framework.Mapping;
+import etu2019.framework.ModelView;
 import etu2019.framework.annotation.App;
+import etu2019.framework.annotation.ControllerA;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -23,34 +25,61 @@ import java.util.List;
  * @author koloina
  */
 public class FrontServlet extends HttpServlet {
-     HashMap<String,Mapping> MappingUrls;
+     HashMap<String,Mapping> mappingUrls= new HashMap<String, Mapping>();
+
      
         @Override
     public void init() throws ServletException{
-       Annote a= new Annote();
-        List<Class<?>> annoted_classes = a.ClassesbyPackages();
-        for (Class<?> c : annoted_classes) {
+            ServletContext context= getServletContext();
+            String p= context.getInitParameter("package");
+            try{
+                 List<Class<?>> annoted_classes =  Annote.getClassesWithAnnotation2(ControllerA.class,p);
+             for (Class<?> c : annoted_classes) {
+                
             Method[] methods = c.getMethods();
             for (Method m : methods) {
                  if (m.isAnnotationPresent(App.class)) {
                     Mapping mapping = new Mapping();
                     mapping.setclassName(c.getName());
                     mapping.setmethod(m.getName());
-                    String url = m.getAnnotation(App.class).url();
-                    this.MappingUrls.put(url,mapping);
+                    App app = m.getAnnotation(App.class);
+                    String url = app.url();
+                    this.mappingUrls.put(url,mapping);
                 }
             }
+            }
+            }catch(Exception e){
+            
+            }
+            
         }
-    }
+    
      
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-             String query = request.getQueryString(); 
-             String urlString = request.getRequestURL().toString() + "?" + query; 
-             out.println("URL: " + urlString);       
-             
+         PrintWriter out = response.getWriter();
+        try  {  
+             String query = request.getQueryString();
+             String urlString = request.getRequestURL().toString(); 
+             //out.print(urlString);
+             String [] tab= urlString.split("/");
+             String url= tab[tab.length-1];
+             if(this.mappingUrls.containsKey(url)){
+                 String className= this.mappingUrls.get(url).getclassName();
+                 String method= this.mappingUrls.get(url).getmethod();
+                 Class<?> c= Class.forName(className);
+                 Method m= c.getDeclaredMethod(method);
+                 Object o= c.newInstance();
+                 ModelView mv= (ModelView) m.invoke(o);
+                 if(mv.getClass() == ModelView.class){
+                     RequestDispatcher dispat = request.getRequestDispatcher(mv.getView());
+                     dispat.forward(request,response);
+                     
+                 }
+             }
+        }catch(Exception e){
+            //e.printStackTrace(out);
         }
     }
 
