7a9329740bfa3aaa4af5b6b3261df9e8aa20b52b
koloina06 <koloina.mihajatiana@gmail.com>
2023-04-06 12:30:46 +0300

    sprint6

--- framework/src/java/etu2019/framework/ModelView.java
+++ framework/src/java/etu2019/framework/ModelView.java
@@ -4,12 +4,15 @@
  */
 package etu2019.framework;
 
+import java.util.HashMap;
+
 /**
  *
  * @author koloina
  */
 public class ModelView {
     String view;
+    HashMap<String,Object> data;
 
     public String getView() {
         return view;
@@ -18,6 +21,18 @@ public class ModelView {
     public void setView(String view) {
         this.view = view;
     }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
     
+    public void addItem(String nom, Object valeur){
+        data = new HashMap<String,Object>();
+        data.put(nom, valeur);
+    }
     
 }
---
+++ testFramework/src/java/model/Emp.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import etu2019.framework.ModelView;
+import etu2019.framework.annotation.App;
+import etu2019.framework.annotation.ControllerA;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author koloina
+ */
+@ControllerA
+public class Emp {
+    String nom;
+    int age;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Emp(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+    }
+    
+    public Emp(){
+    
+    }
+        
+     @App(url="emp-all")
+   public ModelView  findAll(){
+       Emp e1= new Emp("Rabe",30);
+       Emp e2= new Emp("Rasoa",25);
+       ArrayList<Emp> list= new ArrayList<Emp>();
+       list.add(e1);
+       list.add(e2);
+        ModelView mv= new ModelView();
+        mv.setView("test.jsp");
+        mv.addItem("listEmp", list);
+        return mv;
+    }
+}
